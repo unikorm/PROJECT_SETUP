d64a78994f9fe9d200cdf787dadce85653766f0a
unikorm <adaled00@gmail.com>
2023-06-26 18:16:43 +0200

    another minimini

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # PROJECT_SETUP
 setup files and stuff to start quickly new project, it will be more enhance by time and my experiences...
+
+this is like it works:
+adamaanna@MacBook-Air-uzivatela-Adam project_Setup % python3 projectSetUp.py
+Enter the project directory: /Users/adamaanna/documents/www
+Enter the project name: hahaha
+Project structure created successfully!
+
+I execute following commands to create this folder, first i write "python3 projectSetUo.py" command, that call my Python file where is function to create this magic. Then terminal ask me to specific folder to save and then name of creating folder, and that it for now
--- projectSetUp.py
+++ projectSetUp.py
@@ -15,17 +15,14 @@ def copy_template_files(project_dir, project_name):
     shutil.copy(os.path.join(template_dir, "main.js"), os.path.join(project_dir, project_name, "src/main.js"))
     shutil.copy(os.path.join(template_dir, "style.css"), os.path.join(project_dir, project_name, "src/style.css"))
 
-def create_project(project_dir, project_name):
+def create_project():
+    project_dir = input("Enter the project directory path: ")
+    project_name = input("Enter the project name: ")
+    
     create_project_folder(project_dir, project_name)
     create_project_structure(project_dir, project_name)
     copy_template_files(project_dir, project_name)
-    print("Project structure created successfully!")
+    print("Project structure created successfully....  you bitch! Now you must code until your death")
 
-
-# Specify the project directory and name
-project_directory = "/Users/adamaanna/documents/www"
-project_name = "projectSetup"
-
-
-# Call the function with the project directory and name
-create_project(project_directory, project_name)
+# Call the function to create the project interactively
+create_project()
