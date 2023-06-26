c720c5f2d945c9115caeb5a40d60d10b1e05241c
unikorm <adaled00@gmail.com>
2023-06-26 18:57:16 +0200

    continuing

--- projectSetUp.py
+++ projectSetUp.py
@@ -23,16 +23,30 @@ def initialize_git_repository(project_dir, project_name):
     subprocess.run(["git", "add", "."])  # Stage all files
     subprocess.run(["git", "commit", "-m", "Initial commit"])  # Commit the changes
 
+def add_remote_repository(project_dir, project_name, remote_url):
+    os.chdir(os.path.join(project_dir, project_name))  # Change the current working directory to the project directory
+    subprocess.run(["git", "remote", "add", "origin", remote_url])  # Add the remote repository URL
+
+def push_to_remote_repository(project_dir, project_name):
+    os.chdir(os.path.join(project_dir, project_name))  # Change the current working directory to the project directory
+    subprocess.run(["git", "push", "-u", "origin", "main"])  # Push to the remote repository
+
 def create_project():
-    project_dir = input("Enter the project directory: ")
+    project_dir = input("Enter the project directory path: ")
     project_name = input("Enter the project name: ")
+    remote_url = input("Enter URL of your remote repository.")
     
     create_project_folder(project_dir, project_name)
     create_project_structure(project_dir, project_name)
     copy_template_files(project_dir, project_name)
     initialize_git_repository(project_dir, project_name)
+    add_remote_repository(project_dir, project_name, remote_url)
+    push_to_remote_repository(project_dir, project_name)
+
     print("Project structure created successfully.... you bitch! Now you must code until your death")
-    print("Git repository initialized with initial commit. Now you create repository in Github and pair them")
+    print("Git repository initialized with initial commit")
+    print(f"Remote repository {remote_url} added as origin.")
+    print("Initial commit pushed to the remote repository. Now you can continue to program some great stuff")
 
 # Call the function to create the project interactively
 create_project()
