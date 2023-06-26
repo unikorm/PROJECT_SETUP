8788aad12a4d32df719e7fda747133e48a1f56c1
unikorm <adaled00@gmail.com>
2023-06-26 18:39:24 +0200

    some changes and upgrades

--- README.md
+++ README.md
@@ -8,3 +8,28 @@ Enter the project name: hahaha
 Project structure created successfully!
 
 I execute following commands to create this folder, first i write "python3 projectSetUo.py" command, that call my Python file where is function to create this magic. Then terminal ask me to specific folder to save and then name of creating folder, and that it for now
+
+
+
+them:
+git remote add origin <remote_repository_url>
+
+and:
+git push -u origin main
+
+and when it does not work:
+
+P.S.: little help:
+
+How to solve this problem of "! [rejected] master -> master (fetch first)"
+
+->this way->
+
+git fetch origin main
+git merge  main
+git fetch origin main:tmp
+git rebase tmp
+git push origin HEAD:main
+git branch -D tmp
+
+Now everything works well. smile (((((:
--- projectSetUp.py
+++ projectSetUp.py
@@ -1,5 +1,6 @@
 import os
 import shutil
+import subprocess
 
 def create_project_folder(project_dir, project_name):
     os.makedirs(os.path.join(project_dir, project_name))
@@ -15,14 +16,23 @@ def copy_template_files(project_dir, project_name):
     shutil.copy(os.path.join(template_dir, "main.js"), os.path.join(project_dir, project_name, "src/main.js"))
     shutil.copy(os.path.join(template_dir, "style.css"), os.path.join(project_dir, project_name, "src/style.css"))
 
+def initialize_git_repository(project_dir, project_name):
+    os.chdir(os.path.join(project_dir, project_name))  # Change the current working directory to the project directory
+    subprocess.run(["git", "init"])  # Initialize a Git repository
+    subprocess.run(["git", "branch", "-m", "main"]) # Change name of initial branch
+    subprocess.run(["git", "add", "."])  # Stage all files
+    subprocess.run(["git", "commit", "-m", "Initial commit"])  # Commit the changes
+
 def create_project():
-    project_dir = input("Enter the project directory path: ")
+    project_dir = input("Enter the project directory: ")
     project_name = input("Enter the project name: ")
     
     create_project_folder(project_dir, project_name)
     create_project_structure(project_dir, project_name)
     copy_template_files(project_dir, project_name)
-    print("Project structure created successfully....  you bitch! Now you must code until your death")
+    initialize_git_repository(project_dir, project_name)
+    print("Project structure created successfully.... you bitch! Now you must code until your death")
+    print("Git repository initialized with initial commit. Now you create repository in Github and pair them")
 
 # Call the function to create the project interactively
 create_project()
